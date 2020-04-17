1d445b169b11fdd75476b8c2bc6457941c50b96e
Vipul Sinha <47572837+vsasvipul0605@users.noreply.github.com>
2020-04-18 02:51:23 +0530

    Create ArrayListTest.php

---
+++ tests/ArrayListTest.php
@@ -0,0 +1,9 @@
+<?php
+namespace Jaquarh\ArrayList;
+use ArrayObject;
+use PHPUnit_Framework_TestCase;
+class ArrayListTest extends PHPUnit_Framework_TestCase {
+    public function test() {
+        return true;
+    }
+}
